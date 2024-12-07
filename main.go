10ab71de644ca4c720dcb3057ab51db28fe8d77f
Aly Abdelmotaleb <aly.mf.2001@gmail.com>
2024-12-07 15:58:34 +0200

    🏓

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	_ "net/http"
 
+	"github.com/WaleedKhamees/MatchMaker/db"
+	"github.com/gin-gonic/gin"
+	_ "github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDb()
+	server := gin.Default()
+
+	server.Run(":8000")
 }
