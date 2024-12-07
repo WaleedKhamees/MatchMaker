cfaaeb0b97b2609a82d93b2a773d7847be84fccb
Walid-Kh <walid.osama.khamees@gmail.com>
2024-12-07 22:14:17 +0200

    auth middlewares

---
+++ middlewares/EFAAuth.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/WaleedKhamees/MatchMaker/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func EFAAuth(context *gin.Context) {
+	token := context.GetHeader("Authorization")
+
+	username, email, role, err := utils.VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+	}
+
+	if role != "efa" && role != "master" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+	}
+
+	context.Set("username", username)
+	context.Set("email", email)
+	context.Set("role", role)
+	context.Next()
+}
