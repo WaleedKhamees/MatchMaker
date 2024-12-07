4d8dec749cc8d1a529863f731574f5d88ab9ed70
Walid-Kh <walid.osama.khamees@gmail.com>
2024-12-07 18:29:05 +0200

    hashing passwords

---
+++ utils/password.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
+func ComparePassword(password string, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
