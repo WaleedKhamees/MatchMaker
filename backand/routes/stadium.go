5430a0e8877a43c6609283e6aaa7994c8240ec9a
Walid-Kh <walid.osama.khamees@gmail.com>
2024-12-08 21:45:54 +0200

    Add getStadiumById route

--- backand/routes/stadium.go
+++ backand/routes/stadium.go
@@ -19,3 +19,22 @@ func createStadium(context *gin.Context) {
 	}
 	context.JSON(http.StatusCreated, stadium)
 }
+
+func getStadiumById(context *gin.Context) {
+	var stadiumStruct struct {
+		Id int `binding:"required"`
+	}
+
+	if err := context.ShouldBindJSON(&stadiumStruct); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	stadium, err := models.GetStadiumByID(int64(stadiumStruct.Id))
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"stadium": stadium})
+}
